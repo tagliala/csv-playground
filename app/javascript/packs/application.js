8298ba5e0dbf2357a6559d00f8797e221e02db24
Geremia Taglialatela <tagliala.dev@gmail.com>
2021-04-04 19:59:20 +0200

    Webpacker - Simple Form - Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,17 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
+// require("@rails/ujs").start()
 require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+// require("@rails/activestorage").start()
+// require("channels")
+require('@client-side-validations/simple-form/dist/simple-form.bootstrap4.esm')
 
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+ClientSideValidations.callbacks.form.fail = function(form, eventData) {
+  const {
+    settings
+  } = form[0].ClientSideValidations;
+  $('html, body').animate(
+    {scrollTop: form.find(`.${settings.html_settings.wrapper_error_class}`).offset().top}
+  , 250);
+};
