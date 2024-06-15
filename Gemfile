ee38d8625205dc80ffa0d16763f5869d5bfbb450
Geremia Taglialatela <tagliala.dev@gmail.com>
2024-06-15 15:12:31 +0200

    Webpacker - Simple Form - Bootstrap

--- Gemfile
+++ Gemfile
@@ -6,14 +6,13 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '3.3.2'
 
 gem 'client_side_validations', '~> 22.2'
+gem 'client_side_validations-simple_form', '~> 16.0'
 
 gem 'bootsnap', '~> 1.18', require: false
 gem 'jbuilder', '~> 2.12'
-gem 'jquery-rails', '~> 4.4'
 gem 'puma', '~> 6.4'
 gem 'rails', '7.1.3.4'
 gem 'redis', '~> 5.2'
-gem 'sassc'
 gem 'shakapacker', '~> 8.0'
 gem 'sprockets-rails'
 gem 'sqlite3', '~> 1.7'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@
 // that code so it'll be compiled.
 
 import '@hotwired/turbo-rails'
+import '@client-side-validations/simple-form/src/index.bootstrap4'
 import Rails from '@rails/ujs'
 
 Rails.start()
@@ -11,9 +12,9 @@ Rails.start()
 require("@rails/activestorage").start()
 require("channels")
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+ClientSideValidations.callbacks.form.fail = function(form, eventData) {
+  const { settings } = form[0].ClientSideValidations
+  const firstErrorElement = form[0].querySelector(`.${settings.html_settings.error_class}`)
+
+  firstErrorElement.parentElement.scrollIntoView({ behavior: 'smooth' })
+};
